42188217bbf642ea062313d02b3098083c830711
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-10-31 23:23:06 +0300

    Add files via upload

---
+++ PROGRAMING TASKS/pt 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {   
+  double num1, num2, product;
+  //  user to enter their numbers    
+  printf("Enter the first number: ");
+  scanf("%lf", &num1);
+  printf("Enter the second number: ");
+  scanf("%lf", &num2);
+
+  product = num1 * num2; 
+  printf("The product of %.2lf and %.2lf is: %.2lf\n", num1, num2, product);
+  return 0;
+  }
