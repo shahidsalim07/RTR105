69806c7eb2c7d536b244fefe0a0aa85a94cbfc85
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-11-02 18:06:16 +0300

    pt 1.c

--- PROGRAMING TASKS/pt 1.c
+++ PROGRAMING TASKS/pt 1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    unsigned long val1, val2, rezult;
-    printf("First number: ");
-    scanf("%lu", &val1);
-    printf("Second number: ");
-    scanf("%lu", &val2);
-    rezult = val1 * val2;
-    printf("Result: %lu\n", rezult);
+    unsigned long value1, value2, result;
+    printf("Please enter the first number: ");
+    scanf("%lu", &value1);  // Use the correct format specifier %lu for an unsigned long
+    printf("Please enter the second number: ");
+    scanf("%lu", &value2);  // Use the correct format specifier %lu for an unsigned long
+    result = value1 * value2;  // Correct variable names
+    printf("Result: %lu\n", result);  // Correct variable names and format specifier
     return 0;
 }
+
 
 
 
