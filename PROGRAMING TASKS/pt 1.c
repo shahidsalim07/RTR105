d15d4eb6ecdf6bf4c89aec940f20825149907786
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-11-02 17:37:45 +0300

    pt 1.c

--- PROGRAMING TASKS/pt 1.c
+++ PROGRAMING TASKS/pt 1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
  {   
-  double num1, num2, product;
+  long long num1, num2, product;
   //  user to enter their numbers    
   printf("Enter the first number: ");
-  scanf("%lf", &num1);
+  scanf("%lld", &num1);
   printf("Enter the second number: ");
-  scanf("%lf", &num2);
+  scanf("%lld", &num2);
 
   product = num1 * num2; 
-  printf("The product of %.2lf and %.2lf is: %.2lf\n", num1, num2, product);
+  printf("The product of %.lld and %.2lld is: %.2lld\n", num1, num2, product);
   return 0;
   }
