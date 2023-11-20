bfcf6786fe1638d4bfb78146f9755d4217ae638a
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-11-20 19:43:41 +0300

    Add files via upload

---
+++ Untitled-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int main()
+
+ {  char num;
+
+ printf ("enter a natural number:");
+ scanf("%hhd", &num);
+if (num<0,) 
+printf("please enter a non negetive number ")
+return 1 ; 
+ }
+
+ printf("BINARY REPRESENTATION OF %d:", num);
+ int bits=sizeof(char) * 8;
+ for (int 
