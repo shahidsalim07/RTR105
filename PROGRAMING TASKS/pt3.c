41234c2110297e70694620d643e83a02383e85b1
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-11-30 19:15:48 +0300

    pt3.c

--- PROGRAMING TASKS/pt3.c
+++ PROGRAMING TASKS/pt3.c
@@ -2,10 +2,8 @@
 #include <string.h>
 
 int main() {
-    // Declare three integer variables
     int num1, num2, num3;
 
-    // Ask the user to enter three decimal numbers
     printf("Enter the first decimal number: ");
     scanf("%d", &num1);
 
@@ -15,15 +13,12 @@ int main() {
     printf("Enter the third decimal number: ");
     scanf("%d", &num3);
 
-    // Ask the user for the desired sorting order (ascending or descending)
     char sortingOrder[10];
     printf("Enter the sorting order (ascending or descending): ");
     scanf("%s", sortingOrder);
 
-    // Sort the numbers in ascending or descending order based on user input
     int temp;
     if (strcmp(sortingOrder, "ascending") == 0) {
-        // Sort in ascending order
         if (num1 > num2) {
             temp = num1;
             num1 = num2;
