d626532f64b5eb91b827d3f7eb9455cd77345008
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-12-12 13:35:49 +0200

    Add files via upload

---
+++ dec2bin.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+   int num, bin=0 , rem =0,place=1;
+
+   printf("enter a decimal number\n");
+   scanf("%d", &num);
+
+   printf("the binary no of the given %d is ", num);
+   while (num)
+
+    {
+        rem = num % 2;
+        num = num / 2;
+        bin = bin + (rem *place);
+        place = place * 10 ;
+
+    }
+    printf ("%d\n", bin);
+
+    return 0;
+
+}
