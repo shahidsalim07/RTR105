90808f5d13653c8995a1da9a79d055fd0b58d5fd
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2023-12-22 14:54:42 +0300

    pt3.c

--- PROGRAMING TASKS/pt3.c
+++ PROGRAMING TASKS/pt3.c
@@ -1,67 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    int num1, num2, num3;
+  char letter[3];
+  char order;
 
-    printf("Enter the first decimal number: ");
-    scanf("%d", &num1);
+  printf("Enter the first letter: ");
+  scanf(" %c", &letter[0]);
 
-    printf("Enter the second decimal number: ");
-    scanf("%d", &num2);
+  printf("Enter the second letter: ");
+  scanf(" %c", &letter[1]);
 
-    printf("Enter the third decimal number: ");
-    scanf("%d", &num3);
+  printf("Enter the third letter: ");
+  scanf(" %c", &letter[2]);
 
-    char sortingOrder[10];
-    printf("Enter the sorting order (ascending or descending): ");
-    scanf("%s", sortingOrder);
+  printf("Enter A for ascending order or D for descending order: ");
+  scanf(" %c", &order);
 
-    int temp;
-    if (strcmp(sortingOrder, "ascending") == 0) {
-        if (num1 > num2) {
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
-        }
+  if (order == 'A') {
+    printf("Sorted letters: %c %c %c\n", letter[0], letter[1], letter[2]);
+  } else {
+    printf("Sorted letters: %c %c %c\n", letter[2], letter[1], letter[0]);
+  }
 
-        if (num1 > num3) {
-            temp = num1;
-            num1 = num3;
-            num3 = temp;
-        }
-
-        if (num2 > num3) {
-            temp = num2;
-            num2 = num3;
-            num3 = temp;
-        }
-    } else if (strcmp(sortingOrder, "descending") == 0) {
-        // Sort in descending order
-        if (num1 < num2) {
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
-        }
-
-        if (num1 < num3) {
-            temp = num1;
-            num1 = num3;
-            num3 = temp;
-        }
-
-        if (num2 < num3) {
-            temp = num2;
-            num2 = num3;
-            num3 = temp;
-        }
-    } else {
-        printf("Invalid sorting order. Please enter 'ascending' or 'descending'.\n");
-        return 1;
-    }
-
-    // Print the sorted sequence
-    printf("Sorted sequence: %d, %d, %d\n", num1, num2, num3);
-
-    return 0;
+  return 0;
 }
