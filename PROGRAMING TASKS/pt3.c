500811f047f12d3f1a203a3d8fe379d46269d958
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-01-25 18:26:36 +0200

    pt3.c

--- PROGRAMING TASKS/pt3.c
+++ PROGRAMING TASKS/pt3.c
@@ -4,7 +4,6 @@ int main()
 {
     char letter1, letter2, letter3;
 
-    // User input for three letters
     printf("Enter the first letter: ");
     scanf(" %c", &letter1);
 
@@ -16,14 +15,11 @@ int main()
 
     char order;
 
-    // User input for order preference
-    printf("Enter 'a' for alphabetical order or 'd' for decsending order: ");
+    printf("Enter 'a' for alphabetical order or 'd' for descending order: ");
     scanf(" %c", &order);
 
-    // Sorting logic
     if (letter1 > letter2) 
     {
-        // Swap letter1 and letter2 using a temporary variable
         char temp = letter1;
         letter1 = letter2;
         letter2 = temp;
@@ -31,7 +27,6 @@ int main()
 
     if (letter2 > letter3) 
     {
-        // Swap letter2 and letter3 using a temporary variable
         char temp = letter2;
         letter2 = letter3;
         letter3 = temp;
@@ -39,20 +34,18 @@ int main()
 
     if (letter1 > letter2) 
     {
-        // Double-check and swap letter1 and letter2 if needed
         char temp = letter1;
         letter1 = letter2;
         letter2 = temp;
     }
 
-    // Output based on user's order preference
     if (order == 'a') 
     { 
         printf("Alphabetical Order for these Letters: \n%c, %c, %c\n", letter1, letter2, letter3);
     } 
     else if (order == 'd') 
     {
-        printf("descending order for these Letters: \n%c, %c, %c\n", letter3, letter2, letter1);
+        printf("Descending Order for these Letters: \n%c, %c, %c\n", letter3, letter2, letter1);
     }
 
     return 0;
