7bd4834706a6effa57f99980dd99e20d431536c3
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-02-23 13:37:34 +0200

    pt4.c

--- PROGRAMING TASKS/pt4.c
+++ PROGRAMING TASKS/pt4.c
@@ -1,50 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    // Declare variables
-    int number;
+    double factorial = 1.0;
+    double result = 1.0;
+    double number;
     char dataType;
-    long long factorial;
 
-    // Ask the user to enter a decimal number
     printf("Enter a decimal number: ");
-    scanf("%d", &number);
+    scanf("%lf", &number);
 
-    // Ask the user to select the data type (char, int, or long long)
-    printf("Select the data type to use (char, int, or long long): ");
+    printf("Choose data type - char (c), int (i), or long long (l): ");
     scanf(" %c", &dataType);
 
-    // Calculate the factorial based on the selected data type
-    if (dataType == 'c') {
-        // Use the `char` data type
-        for (factorial = 1; factorial <= number; factorial++) {
-            if (factorial > (char) 127) {
-                printf("Cannot calculate the factorial of %d using 'char' data type.\n", number);
-                return 1;
+    switch(dataType) {
+        case 'c':
+            while (number > 0 && result <= factorial) {
+                factorial *= number;
+                result = factorial;
+                number--;
             }
-        }
-        printf("Factorial of %d using 'char' data type: %lld\n", number, factorial);
-    } else if (dataType == 'i') {
-        // Use the `int` data type
-        for (factorial = 1; factorial <= number; factorial++) {
-            if (factorial > (int) 32767) {
-                printf("Cannot calculate the factorial of %d using 'int' data type.\n", number);
-                return 1;
+            break;
+        case 'i':
+            while (number > 0 && result <= factorial) {
+                factorial *= number;
+                result = factorial;
+                number--;
             }
-        }
-        printf("Factorial of %d using 'int' data type: %lld\n", number, factorial);
-    } else if (dataType == 'l') {
-        // Use the `long long` data type
-        for (factorial = 1; factorial <= number; factorial++) {
-            if (factorial > (long long) 9223372036854775807) {
-                printf("Cannot calculate the factorial of %d using 'long long' data type.\n", number);
-                return 1;
+            break;
+        case 'l':
+            while (number > 0 && result <= factorial) {
+                factorial *= number;
+                result = factorial;
+                number--;
             }
-        }
-        printf("Factorial of %d using 'long long' data type: %lld\n", number, factorial);
+            break;
+        default:
+            printf("Invalid data type selected.\n");
+            return 1;
+    }
+
+    if (result > factorial) {
+        printf("Factorial value calculation with the selected data type is not possible.\n");
     } else {
-        printf("Invalid data type selection. Please enter 'c', 'i', or 'l'.\n");
-        return 1;
+        printf("Factorial of %.0lf is: %.0lf\n", number + 1, factorial);
     }
 
     return 0;
