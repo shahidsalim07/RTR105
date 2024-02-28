86ec1b4d1a3592cac3d080b52dc99e25ce260e19
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-02-28 14:49:52 +0200

    pt4 for defence.c

---
+++ PROGRAMING TASKS/pt4 for defence.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void calculateAndPrintFactorial(char datatype, char num);
+
+int main() {
+    char datatype;
+    char num;
+
+    while (1) {
+        printf("Enter a decimal number (or enter -1 to exit): ");
+        scanf("%hhd", &num);
+
+        if (num == -1) {
+            printf("Exiting the program.\n");
+            break;
+        }
+
+        while (getchar() != '\n');
+
+        printf("Enter a datatype (1 for char, 2 for int, or 3 for long long): ");
+        scanf(" %c", &datatype); // note the space before %c to consume the newline character
+
+        calculateAndPrintFactorial(datatype, num);
+    }
+
+    return 0;
+}
+
+void calculateAndPrintFactorial(char datatype, char num) {
+    if (datatype == '1') {
+        // Handle char datatype
+        char result = 1;
+
+        for (char i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        printf("Factorial of the given number using char is: %d.\n", result);
+    } else {
+        // Handle other datatypes
+        printf("Error: Factorial calculation is not implemented for the specified datatype.\n");
+    }
+}
