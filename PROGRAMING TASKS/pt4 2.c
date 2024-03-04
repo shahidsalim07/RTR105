0d45601bde74367ea045fd64331e745b411dafcd
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-03-04 13:09:45 +0200

    pt4 2.c

--- PROGRAMING TASKS/pt4 2.c
+++ PROGRAMING TASKS/pt4 2.c
@@ -18,7 +18,7 @@ int main() {
         while (getchar() != '\n');
 
         printf("Enter a datatype (1 for char, 2 for int, or 3 for long long): ");
-        scanf(" %c", &datatype); 
+        scanf(" %c", &datatype);
 
         calculateAndPrintFactorial(datatype, num);
     }
@@ -28,24 +28,19 @@ int main() {
 
 void calculateAndPrintFactorial(char datatype, char num) {
     if (datatype == '1') {
-
         char result = 1;
 
         for (char i = 1; i <= num; i++) {
             result *= i;
-            
-            
-            if (result < 0) {
-                printf("Error: Factorial result exceeds the capacity of char datatype.\n");
-                return;
-            }
-
             printf("Intermediate result after multiplying by %d is: %d.\n", i, result);
         }
 
-        printf("Factorial of the given number using char is: %d.\n", result);
+        if (result < 0) {
+            printf("Error: Factorial result becomes negative for char datatype.\n");
+        } else {
+            printf("Factorial of the given number using char is: %d.\n", result);
+        }
     } else {
-        // Handle other datatypes
         printf("Error: Factorial calculation is not implemented for the specified datatype.\n");
     }
 }
