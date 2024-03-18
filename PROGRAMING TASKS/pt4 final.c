d6356784c5e6b0f058e2aa045eaa6fc7a2ce09ed
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-03-18 18:36:13 +0200

    pt4 final.c

--- PROGRAMING TASKS/pt4 final.c
+++ PROGRAMING TASKS/pt4 final.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <limits.h>
 
 void calculateFactorial(int num, char type) {
     if (type == 'i') {
         int result = 1;
         for (int i = 1; i <= num; ++i) {
-            if (result > INT_MAX / i) {
+            if (result > 2147483647 / i) { // Check for overflow
                 printf("Overflow occurred at %d!\n", i);
                 break;
             }
@@ -15,22 +14,22 @@ void calculateFactorial(int num, char type) {
     } else if (type == 'c') {
         char result = 1;
         for (int i = 1; i <= num; ++i) {
-            result *= i;
-            printf("Factorial of %d is %d\n", i, result);
-            if (result < 0) {
+            if (result > 127 / i) { // Check for overflow
                 printf("Overflow occurred at %d!\n", i);
                 break;
             }
+            result *= i;
+            printf("Factorial of %d is %d\n", i, result);
         }
     } else if (type == 'l') {
         long long result = 1;
         for (int i = 1; i <= num; ++i) {
-            result *= i;
-            printf("Factorial of %d is %lld\n", i, result);
-            if (result < 0) {
+            if (result > 9223372036854775807LL / i) { // Check for overflow
                 printf("Overflow occurred at %d!\n", i);
                 break;
             }
+            result *= i;
+            printf("Factorial of %d is %lld\n", i, result);
         }
     } else {
         printf("Invalid data type selected!\n");
