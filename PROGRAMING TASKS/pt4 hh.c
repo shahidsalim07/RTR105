8ed3768768387729c069b9c9a8026731ce98eb26
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-04-08 17:23:46 +0300

    pt4 hh.c

---
+++ PROGRAMING TASKS/pt4 hh.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    char choice;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("Choose data type:\n");
+    printf("1. int\n");
+    printf("2. char\n");
+    printf("3. long long\n");
+    printf("Enter your choice (1, 2, or 3): ");
+    scanf(" %c", &choice);
+
+    switch (choice) {
+        case '1': {
+            int i;
+            int result = 1;
+            int prev_result;
+            for (i = 1; i <= num; i++) {
+                prev_result = result;
+                result *= i;
+                if (result / i != prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %d\n", i, result);
+            }
+            break;
+        }
+        case '2': {
+            int i;
+            char result = 1;
+            char prev_result;
+            for (i = 1; i <= num; i++) {
+                prev_result = result;
+                result *= i;
+                if ((char)(result / i) != (char)prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %d\n", i, result);
+            }
+            break;
+        }
+        case '3': {
+            int i;
+            long long result = 1;
+            long long prev_result;
+            for (i = 1; i <= num; i++) {
+                prev_result = result;
+                result *= i;
+                if (result / i != prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %lld\n", i, result);
+            }
+            break;
+        }
+        default:
+            printf("Invalid choice.\n");
+            break;
+    }
+
+    return 0;
+}
