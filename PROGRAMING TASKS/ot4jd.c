8bf2745ff0adc8e2b02ec806c32424b1d943fd10
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-04-08 18:40:22 +0300

    Update ot4jd.c

--- PROGRAMING TASKS/ot4jd.c
+++ PROGRAMING TASKS/ot4jd.c
@@ -1,38 +1,71 @@
 #include <stdio.h>
 
-unsigned long long factorial(unsigned long long n);
-unsigned long long previous_answer(unsigned long long current_answer, unsigned long long n);
-int is_condition_valid(unsigned long long current_answer, unsigned long long prev_answer, unsigned long long n);
+unsigned long long factorial_ulonglong(int num) {
+    unsigned long long result = 1;
+    unsigned long long prevResult = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+        if (result < prevResult) {
+            printf("Overflow! Factorial of %d is out of range for unsigned long long data type.\n", num);
+            break;
+        }
+        prevResult = result;
+    }
+    return result;
+}
+
+long long factorial_longlong(int num) {
+    long long result = 1;
+    long long prevResult = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+        if (result < prevResult) {
+            printf("Overflow! Factorial of %d is out of range for long long data type.\n", num);
+            break;
+        }
+        prevResult = result;
+    }
+    return result;
+}
+
+int factorial_int(int num) {
+    int result = 1;
+    int prevResult = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+        if (result < prevResult) {
+            printf("Overflow! Factorial of %d is out of range for int data type.\n", num);
+            break;
+        }
+        prevResult = result;
+    }
+    return result;
+}
 
 int main() {
-    unsigned long long number, current_answer, prev_answer;
+    int num;
+    char choice;
 
-    printf("Enter a positive number to calculate its factorial: ");
-    scanf("%llu", &number);
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    current_answer = factorial(number);
-    prev_answer = previous_answer(current_answer, number);
+    printf("Choose datatype for factorial calculation:\n");
+    printf("1. char\n2. int\n3. long long\n");
+    scanf(" %c", &choice);
 
-    if (is_condition_valid(current_answer, prev_answer, number)) {
-        printf("The factorial of %llu is: %llu\n", number, current_answer);
-        printf("The previous answer obtained by dividing the current answer by its factorial is: %llu\n", prev_answer);
-    } else {
-        printf("Error: The condition is not valid.\n");
+    switch (choice) {
+        case '1':
+            printf("Factorial of %d = %d\n", num, factorial_int(num));
+            break;
+        case '2':
+            printf("Factorial of %d = %d\n", num, factorial_int(num));
+            break;
+        case '3':
+            printf("Factorial of %d = %lld\n", num, factorial_longlong(num));
+            break;
+        default:
+            printf("Invalid choice\n");
     }
 
     return 0;
-}
-
-unsigned long long factorial(unsigned long long n) {
-    if (n == 0) return 1;
-    return n * factorial(n - 1);
-}
-
-unsigned long long previous_answer(unsigned long long current_answer, unsigned long long n) {
-    if (n == 0) return 0; // Division by zero is not defined
-    return current_answer / n;
-}
-
-int is_condition_valid(unsigned long long current_answer, unsigned long long prev_answer, unsigned long long n) {
-    return (current_answer / factorial(n)) == prev_answer;
 }
