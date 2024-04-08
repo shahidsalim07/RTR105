5ee68ff4f67517a2ab525e49eb76ee58e40dc360
shahidsalim07 <134400039+shahidsalim07@users.noreply.github.com>
2024-04-08 17:31:20 +0300

    pt5.c

--- PROGRAMING TASKS/pt5.c
+++ PROGRAMING TASKS/pt5.c
@@ -1,54 +1,70 @@
 #include <stdio.h>
 
 int main() {
-    // Declare variables
-    int number;
-    char dataType;
-    unsigned long long factorial;
+    int num;
+    char choice;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("Choose data type:\n");
+    printf("1. int\n");
+    printf("2. char\n");
+    printf("3. long long\n");
+    printf("Enter your choice (1, 2, or 3): ");
+    scanf(" %c", &choice);
 
-    // Ask the user to enter a decimal number
-    printf("Enter a decimal number: ");
-    scanf("%d", &number);
-
-    // Ask the user to select the data type (char, int, or long long)
-    printf("Select the data type to use (char, int, or long long): ");
-    scanf(" %c", &dataType);
-
-    // Calculate the factorial based on the selected data type
-    if (dataType == 'c') {
-        // Use the `char` data type
-        factorial = 1;
-        for (; factorial <= number; factorial++) {
-            if (factorial > (unsigned char) 127) {
-                printf("Cannot calculate the factorial of %d using 'char' data type.\n", number);
-                return 1;
+    switch (choice) {
+        case '1': {
+            int i = 1;
+            int result = 1;
+            int prev_result;
+            while (i <= num) {
+                prev_result = result;
+                result *= i;
+                if (result / i != prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %d\n", i, result);
+                i++;
             }
+            break;
         }
-        printf("Factorial of %d using 'char' data type: %llu\n", number, factorial);
-    } else if (dataType == 'i') {
-        // Use the `int` data type
-        factorial = 1;
-        for (; factorial <= number; factorial++) {
-            if (factorial > (unsigned int) 32767) {
-                printf("Cannot calculate the factorial of %d using 'int' data type.\n", number);
-                return 1;
+        case '2': {
+            int i = 1;
+            char result = 1;
+            char prev_result;
+            while (i <= num) {
+                prev_result = result;
+                result *= i;
+                if ((char)(result / i) != (char)prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %d\n", i, result);
+                i++;
             }
+            break;
         }
-        printf("Factorial of %d using 'int' data type: %llu\n", number, factorial);
-    } else if (dataType == 'l') {
-        // Use the `long long` data type
-        factorial = 1;
-        for (; factorial <= number; factorial++) {
-            // Check for overflow without comparing to zero
-            if ((factorial >> 63) && ((factorial & ~(~(unsigned long long) 0)) == 1)) {
-                printf("Cannot calculate the factorial of %d using 'long long' data type.\n", number);
-                return 1;
+        case '3': {
+            int i = 1;
+            long long result = 1;
+            long long prev_result;
+            while (i <= num) {
+                prev_result = result;
+                result *= i;
+                if (result / i != prev_result) {
+                    printf("Overflow occurred!\n");
+                    return 0;
+                }
+                printf("Result of %d! = %lld\n", i, result);
+                i++;
             }
+            break;
         }
-        printf("Factorial of %d using 'long long' data type: %llu\n", number, factorial);
-    } else {
-        printf("Invalid data type selection. Please enter 'c', 'i', or 'l'.\n");
-        return 1;
+        default:
+            printf("Invalid choice.\n");
+            break;
     }
 
     return 0;
